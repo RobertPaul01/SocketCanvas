5327423c23a84618d2a597dd3f3bdd64364ff47e
Robby <rgilbertpaul@gmail.com>
2016-10-12 16:51:25 -0500

    Init with drawing working

---
+++ SocketCanvas/DrawViewController.swift
@@ -0,0 +1,105 @@
+//
+//  ViewController.swift
+//  SocketCanvas
+//
+//  Created by Robert Paul on 10/12/16.
+//  Copyright © 2016 Robert Paul. All rights reserved.
+//
+
+import UIKit
+
+class DrawViewController: UIViewController {
+    
+    // MARK: Color settings
+    var brushColor: CGColor = UIColor.black.cgColor
+    var brushWidth: CGFloat = 10.0
+    var alpha: CGFloat = 1.0
+    
+    // MARK: Drawing vars
+    var lastPoint = CGPoint.zero
+    var swiped = false
+    
+    // MARK: Outlets
+    @IBOutlet weak var tempImageView: UIImageView!
+    @IBOutlet weak var mainImageView: UIImageView!
+    
+    // MARK: Drawing functions
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        swiped = false
+        if let touch = touches.first {
+            lastPoint = touch.location(in: self.view)
+        }
+    }
+    
+    func drawLineFrom(fromPoint: CGPoint, toPoint: CGPoint, with color: CGColor) {
+        
+        UIGraphicsBeginImageContext(view.frame.size)
+        if let context = UIGraphicsGetCurrentContext() {
+            tempImageView.image?.draw(in: CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height))
+            
+            context.move(to: fromPoint)
+            context.addLine(to: toPoint)
+            
+            context.setLineCap(CGLineCap.round)
+            context.setLineWidth(brushWidth)
+            
+            context.setStrokeColor(color)
+            context.setBlendMode(CGBlendMode.normal)
+            
+            context.strokePath()
+            
+            tempImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+            tempImageView.alpha = alpha
+        } else {
+            print("Error: Drawing context not found!!!")
+        }
+        UIGraphicsEndImageContext()
+
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        swiped = true
+        if let touch = touches.first {
+            let currentPoint = touch.location(in: view)
+            drawLineFrom(fromPoint: lastPoint, toPoint: currentPoint, with: brushColor)
+            
+            lastPoint = currentPoint
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if !swiped {
+            // draw a single point
+            drawLineFrom(fromPoint: lastPoint, toPoint: lastPoint, with: brushColor)
+        }
+        
+        // Merge tempImageView into mainImageView
+        UIGraphicsBeginImageContext(mainImageView.frame.size)
+        
+        mainImageView.image?.draw(in: CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height),
+                                  blendMode: CGBlendMode.normal,
+                                  alpha: 1.0)
+        
+        tempImageView.image?.draw(in: CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height),
+                                  blendMode: CGBlendMode.normal,
+                                  alpha: alpha)
+        
+        mainImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        tempImageView.image = nil
+    }
+
+    // MARK: Boilerplate
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
+
